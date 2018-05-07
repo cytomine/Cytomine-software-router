90f8355013a14391a058e81ea4abb8ac85e68d6a
GiMichiels <gino_michiels@hotmail.com>
2018-05-07 16:38:45 +0200

    Add a default refresh rate and the possibility to override it

--- src/main/java/be/cytomine/software/repository/threads/RepositoryManagerThread.groovy
+++ src/main/java/be/cytomine/software/repository/threads/RepositoryManagerThread.groovy
@@ -16,35 +16,30 @@ package be.cytomine.software.repository.threads
  * limitations under the License.
  */
 
+import be.cytomine.software.consumer.Main
 import be.cytomine.software.repository.SoftwareManager
-
-/*
- * Copyright (c) 2009-2018. Authors: see NOTICE file.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 import groovy.util.logging.Log4j
 
 @Log4j
 class RepositoryManagerThread implements Runnable {
 
-    def refreshRate = 300
+    private final DEFAULT_REFRESH_RATE = 300
+
+    def refreshRate
     def index = 0
     def repositoryManagers = []
 
     @Override
     void run() {
+        def result = Main.configFile.repositoryManagerRefreshRate
+        if (result.getClass().getName() == "groovy.util.ConfigObject" && result.isEmpty()) {
+            refreshRate = DEFAULT_REFRESH_RATE
+        } else  {
+            refreshRate = result
+        }
+
+        log.info("Refresh rate (seconds) : ${refreshRate}")
+
         while (true) {
             log.info("Amount of repository managers : ${repositoryManagers.size()}")
 
@@ -55,9 +50,9 @@ class RepositoryManagerThread implements Runnable {
                 (repositoryManagers.get(index) as SoftwareManager).updateSoftware()
 
                 index = ++index % repositoryManagers.size()
-
-                sleep((refreshRate as Long) * 1000)
             }
+
+            sleep((refreshRate as Long) * 1000)
         }
     }
 

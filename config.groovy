36d40b71b52709d10aa9841f4d41621cf53d3dea
Ulysse Rubens <ulyrubens@hotmail.com>
2018-06-11 15:39:05 +0200

    Generic default configuration

--- config.groovy
+++ config.groovy
@@ -1,7 +1,8 @@
 cytomineCoreURL = "http://localhost:8080"
 
-publicKey = "c48a6252-26ad-408f-989a-df311efe0d3b"
-privateKey = "1003b5dc-b680-4106-b35b-faab0637f2b6"
+// RabbitMQ Cytomine user (must be super admin)
+publicKey = "XXX"
+privateKey = "XXX"
 
 rabbitAddress = "localhost"
 rabbitPort = "5672"
@@ -11,11 +12,10 @@ rabbitPassword = "router"
 exchangeCommunication = "exchangeCommunication"
 queueCommunication = "queueCommunication"
 
-jobDirectory = "./"
 logsDirectory = "algo/logs/"
 dataDirectory = "algo/data/"
 imagesDirectory = "algo/images/"
 
 softwareDescriptorName = "descriptor.json"
 
-keyFilePath = "/home/gmichiels/.ssh/id_rsa"
+keyFilePath = ".ssh/id_rsa"

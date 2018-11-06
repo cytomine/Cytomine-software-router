a77d4d9c02bd49a80c65444f3f98a9aa1f509675
Ulysse Rubens <ulyrubens@hotmail.com>
2018-11-06 18:04:55 +0100

    Add Travis integration

---
+++ continuous_integration/deploy.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+git fetch --tags
+export VERSION=$(mvn -q -Dexec.executable=echo -Dexec.args='${project.version}' --non-recursive exec:exec)
+export DEPLOY=false
+if [[ ! $(git tag -l v$VERSION) ]]; then
+    git config --local user.name "$(git log -1 --pretty=format:'%an')"
+    git config --local user.email "$(git log -1 --pretty=format:'%ae')"
+    git tag "v$VERSION"
+    export DEPLOY=true
+    echo "Deploy with version $VERSION"
+
+    mvn package
+    cp log4j.properties docker/
+    cp target/cytomine-software-router-$VERSION-jar-with-dependendies.jar docker/
+    docker build --build-arg RELEASE_PATH="." -t cytomineuliege/software_router:latest -t cytomineuliege/software_router:v$VERSION docker/
+fi;
--- docker/deploy.sh
+++ docker/deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-#
-# Copyright (c) 2009-2017. Authors: see NOTICE file.
+
+# Copyright (c) 2009-2018. Authors: see NOTICE file.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -13,54 +13,13 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-#
-
 
 bash /tmp/addHosts.sh
 
+touch /tmp/log.out
 
-cd /software_router/algo/
-cp -R /root/Cytomine/Cytomine-python-datamining/cytomine-applications/classification_model_builder .
-cp -R /root/Cytomine/Cytomine-python-datamining/cytomine-applications/classification_prediction .
-cp -R /root/Cytomine/Cytomine-python-datamining/cytomine-applications/classification_validation .
-cp -R /root/Cytomine/Cytomine-python-datamining/cytomine-datamining .
-cp -R /root/Cytomine/Cytomine-python-datamining/cytomine-applications/detect_sample .
-cp -R /root/Cytomine/Cytomine-python-datamining/cytomine-applications/export_landmark .
-cp -R /root/Cytomine/Cytomine-python-datamining/cytomine-applications/landmark_model_builder .
-cp -R /root/Cytomine/Cytomine-python-datamining/cytomine-applications/landmark_prediction .
-cp -R /root/Cytomine/Cytomine-python-datamining/cytomine-applications/object_finder .
-cp -R /root/Cytomine/Cytomine-python-datamining/cytomine-applications/segmentation_model_builder .
-cp -R /root/Cytomine/Cytomine-python-datamining/cytomine-applications/segmentation_prediction .
-cp -R /root/Cytomine/Cytomine-python-datamining/cytomine-applications/ldm_model_builder .
-cp -R /root/Cytomine/Cytomine-python-datamining/cytomine-applications/ldm_prediction .
+cd /opt/
 
+java -jar cytomine-software-router.jar > /tmp/log.out &
 
-
-#mkdir -p /software_router/algo/simple_elastix
-#mv /tmp/get_and_move.py /software_router/algo/simple_elastix/get_and_move.py
-
-cp -R /root/Cytomine/Cytomine-tools/computeAnnotationStats .
-cp /root/Cytomine/Cytomine-tools/computeTermArea.groovy .
-mkdir ../lib
-cp -R /root/Cytomine/Cytomine-tools/jars ../lib
-cp /root/Cytomine/Cytomine-tools/union4.groovy ../lib
-
-cd /software_router/
-mv cytomine-java-client.jar lib/jars/Cytomine-client-java.jar
-mv /tmp/injectSoftware.groovy .
-
-# horrible hack for groovy with dash
-PATH="$PATH:$GROOVY_HOME/bin:/root/anaconda/bin"
-
-
-groovy -cp 'lib/jars/Cytomine-client-java.jar' injectSoftware.groovy $CORE_URL $RABBITMQ_PUB_KEY $RABBITMQ_PRIV_KEY
-
-
-touch /tmp/test.out
-
-java -jar Cytomine-software-router.jar > /tmp/test.out &
-
-bash wrapdocker #&& service docker start
-
-
-tail -f /tmp/test.out
+tail -f /tmp/log.out

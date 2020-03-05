76cf50630f1417ebe7269c8ceee8d1b1d3e65f13
Ulysse Rubens <urubens@uliege.be>
2020-03-05 15:13:51 +0100

    Fix Bug with synchronized lock on Main.pendingPullingTable object #6

--- src/main/groovy/be/cytomine/software/consumer/threads/ProcessingServerThread.groovy
+++ src/main/groovy/be/cytomine/software/consumer/threads/ProcessingServerThread.groovy
@@ -99,23 +99,22 @@ class ProcessingServerThread implements Runnable {
                         try {
                             Main.cytomine.changeStatus(jobId, Cytomine.JobStatus.WAIT, 0, "Try to find image [${imageName}]")
                         } catch (Exception e) {}
-                        synchronized (Main.pendingPullingTable) {
-                            def start = System.currentTimeSeconds()
-                            while (Main.pendingPullingTable.contains(imageName)) {
-                                def status = "The image [${imageName}] is currently being pulled ! Wait..."
-                                log.warn("${logPrefix} ${status}")
-                                try {
-                                    Main.cytomine.changeStatus(jobId, Cytomine.JobStatus.WAIT, 0, status)
-                                } catch (Exception e) {}
 
-                                if (System.currentTimeSeconds() - start > 1800) {
-                                    status = "A problem occurred during the pulling process !"
-                                    Main.cytomine.changeStatus(jobId, Cytomine.JobStatus.FAILED, 0, status)
-                                    return
-                                }
+                        def start = System.currentTimeSeconds()
+                        while (Main.pendingPullingTable.contains(imageName)) {
+                            def status = "The image [${imageName}] is currently being pulled ! Wait..."
+                            log.warn("${logPrefix} ${status}")
+                            try {
+                                Main.cytomine.changeStatus(jobId, Cytomine.JobStatus.WAIT, 0, status)
+                            } catch (Exception e) {}
 
-                                sleep(60000)
+                            if (System.currentTimeSeconds() - start > 1800) {
+                                status = "A problem occurred during the pulling process !"
+                                Main.cytomine.changeStatus(jobId, Cytomine.JobStatus.FAILED, 0, status)
+                                return
                             }
+
+                            sleep(60000)
                         }
 
                         def imageExists = new File("${Main.configFile.cytomine.software.path.softwareImages}/${imageName}").exists()
